ec787590ebb30f8480b2b9499c7ba8d4cef9e0f4
Ponzis <ponzis.online@gmail.com>
2025-04-06 01:28:46 -0400

    Add light map texture and mask scene, update shader and UI for item interaction

--- scripts/game_state.gd
+++ scripts/game_state.gd
@@ -2,6 +2,7 @@ extends Node
 
 signal pickup
 
+@export var player_name: String = "Bob"
 @export var score: int = 0
 
 @export var item: Item:
--- scripts/ui.gd
+++ scripts/ui.gd
@@ -1,16 +1,16 @@
 extends Control
 
-@onready var hand_label: Label = $Bottom/Hand
+@onready var hand_icon: TextureRect = $Bottom/Hand
 
 func _ready() -> void:
 	GameState.pickup.connect(_on_item_change)
 	
-func _on_item_change(item):
-	if item != null:
-		print("Holding %s." % item.name)
-		hand_label.text = "Holding %s." % item.name
+func _on_item_change(item: Item):
+	if item == null:
+		hand_icon.texture = null
 		return
-	hand_label.text = "Holding nothing."
+		
+	hand_icon.texture = item.texture
 
 
 func _on_reset_pressed() -> void:
